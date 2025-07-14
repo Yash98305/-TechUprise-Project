aed1930d187005de1ba7d44a43a60514ced00763
yash98305 <yash.patel98305@gmail.com>
2025-07-14 21:59:12 +0530

    c7

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const app = express();
 app.use(express.json());
 const corsOptions = {
-    origin: "https://techuprise-yashpatel.netlify.app",
+    origin: ["https://techuprise-yashpatel.netlify.app","http://localhost:3000/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
     credentials: true,
 };
